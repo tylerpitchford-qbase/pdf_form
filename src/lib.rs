c6f04767f33b242fb4ef519292ea295cb0a569de
tylerpitchford-qbase <54452535+tylerpitchford-qbase@users.noreply.github.com>
2020-03-30 16:23:51 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,300 +1,68 @@
-//! This crate is for filling out PDFs with forms programatically.
-extern crate lopdf;
-#[macro_use]
-extern crate bitflags;
-#[macro_use]
-extern crate derive_error;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Response};
+use web_sys::console;
+//use js_sys::Uint8Array;
+use pdf_form_ids::{Form, FieldState};
 
-use lopdf::{Document, Object, ObjectId, StringFormat};
-use std::collections::VecDeque;
-use std::io;
-use std::io::Write;
-use std::path::Path;
-use std::str;
-
-bitflags! {
-    struct ButtonFlags: u32 {
-        const NO_TOGGLE_TO_OFF  = 0x8000;
-        const RADIO             = 0x10000;
-        const PUSHBUTTON        = 0x20000;
-        const RADIO_IN_UNISON   = 0x4000000;
-
-    }
+// Called when the wasm module is instantiated
+#[wasm_bindgen(start)]
+pub fn main() -> Result<(), JsValue> {
+    console::log_1(&"START".into());
+    //run();
+    console::log_1(&"END".into());
+    Ok(())
 }
 
-bitflags! {
-    struct ChoiceFlags: u32 {
-        const COBMO             = 0x20000;
-        const EDIT              = 0x40000;
-        const SORT              = 0x80000;
-        const MULTISELECT       = 0x200000;
-        const DO_NOT_SPELLCHECK = 0x800000;
-        const COMMIT_ON_CHANGE  = 0x8000000;
-    }
-}
+#[wasm_bindgen]
+pub async fn load_data() -> Result<(), JsValue> {
+    console::log_1(&"AAA".into());
 
-/// A PDF Form that contains fillable fields
-///
-/// Use this struct to load an existing PDF with a fillable form using the `load` method.  It will
-/// analyze the PDF and identify the fields. Then you can get and set the content of the fields by
-/// index.
-pub struct Form {
-    doc: Document,
-    form_ids: Vec<ObjectId>,
-}
+    // Use `web_sys`'s global `window` function to get a handle on the global
+    // window object.
+    let window = web_sys::window().expect("no global `window` exists");
+    let document = window.document().expect("should have a document on window");
+    let body = document.body().expect("document should have a body");
 
-/// The possible types of fillable form fields in a PDF
-#[derive(Debug)]
-pub enum FieldType {
-    Button,
-    Radio,
-    CheckBox,
-    ListBox,
-    ComboBox,
-    Text,
-}
+    let window = web_sys::window().unwrap();
+    let pr = window.fetch_with_str("http://localhost:8000/sample.pdf");
 
-/// The current state of a form field
-#[derive(Debug)]
-pub enum FieldState {
-    /// Push buttons have no state
-    Button,
-    /// `selected` is the sigular option from `options` that is selected
-    Radio {
-        selected: String,
-        options: Vec<String>,
-    },
-    /// The toggle state of the checkbox
-    CheckBox { is_checked: bool },
-    /// `selected` is the list of selected options from `options`
-    ListBox {
-        selected: Vec<String>,
-        options: Vec<String>,
-        multiselect: bool,
-    },
-    /// `selected` is the list of selected options from `options`
-    ComboBox {
-        selected: Vec<String>,
-        options: Vec<String>,
-        editable: bool,
-    },
-    /// User Text Input
-    Text { text: String },
-}
+    console::log_1(&"ONE".into());
 
-#[derive(Debug, Error)]
-/// Errors that may occur while loading a PDF
-pub enum LoadError {
-    /// An IO Error
-    IoError(io::Error),
-    /// A dictionary key that must be present in order to find forms was not present
-    DictionaryKeyNotFound,
-    /// The reference `ObjectId` did not point to any values
-    #[error(non_std, no_from)]
-    NoSuchReference(ObjectId),
-    /// An element that was expected to be a reference was not a reference
-    NotAReference,
-    /// A value that must be a certain type was not that type
-    UnexpectedType,
-}
+    let resp_value = JsFuture::from(pr).await?;
 
-/// Errors That may occur while setting values in a form
-#[derive(Debug, Error)]
-pub enum ValueError {
-    /// The method used to set the state is incompatible with the type of the field
-    TypeMismatch,
-    /// One or more selected values are not valid choices
-    InvalidSelection,
-    /// Multiple values were selected when only one was allowed
-    TooManySelected,
-}
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
 
-trait PdfObjectDeref {
-    fn deref<'a>(&self, doc: &'a Document) -> Result<&'a Object, LoadError>;
-}
+    // Convert this other `Promise` into a rust `Future`.
 
-impl PdfObjectDeref for Object {
-    fn deref<'a>(&self, doc: &'a Document) -> Result<&'a Object, LoadError> {
-        match self {
-            &Object::Reference(oid) => doc.objects.get(&oid).ok_or(LoadError::NoSuchReference(oid)),
-            _ => Err(LoadError::NotAReference),
-        }
-    }
-}
+    console::log_1(&"TWO".into());
 
-impl Form {
-    /// Takes a reader containing a PDF with a fillable form, analyzes the content, and attempts to
-    /// identify all of the fields the form has.
-    pub fn load_from<R: io::Read>(reader: R) -> Result<Self, LoadError> {
-        let doc = Document::load_from(reader).unwrap();
-        Self::load_doc(doc)
-    }
+    let buf_val = JsFuture::from(resp.array_buffer().unwrap()).await?;
+    assert!(buf_val.is_instance_of::<js_sys::ArrayBuffer>());
 
-    /// Takes a path to a PDF with a fillable form, analyzes the file, and attempts to identify all
-    /// of the fields the form has.
-    pub fn load<P: AsRef<Path>>(path: P) -> Result<Self, LoadError> {
-        let doc = Document::load(path).unwrap();
-        Self::load_doc(doc)
-    }
+    let typebuf: js_sys::Uint8Array = js_sys::Uint8Array::new(&buf_val);
+    let resp_vec = typebuf.to_vec();
 
-    fn load_doc(doc: Document) -> Result<Self, LoadError> {
-        let mut form_ids = Vec::new();
-        let mut queue = VecDeque::new();
-        // Block so borrow of doc ends before doc is moved into the result
-        {
-            // Get the form's top level fields
-            let catalog = doc
-                .trailer
-                .get(b"Root")
-                .unwrap()
-                .as_dict()
-                .unwrap();
-            let acroform = catalog
-                .get(b"AcroForm")
-                .unwrap()
-                .as_dict()
-                .unwrap();
-            let fields_list = acroform
-                .get(b"Fields")
-                .unwrap()
-                .as_array()
-                .unwrap();
-            queue.append(&mut VecDeque::from(fields_list.clone()));
+    console::log_1(&"THREE".into());
 
-            // Iterate over the fields
-            while let Some(objref) = queue.pop_front() {
-                let obj = objref.deref(&doc)?;
-                if let &Object::Dictionary(ref dict) = obj {
-                    // If the field has FT, it actually takes input.  Save this
-                    match dict.get(b"FT") {
-                        Ok(f) => form_ids.push(objref.as_reference().unwrap()),
-                        _ => (),
-                    }
-                    // If this field has kids, they might have FT, so add them to the queue
-                    match dict.get(b"Kids") {
-                        Ok(f) => queue.append(&mut VecDeque::from(f.clone().as_array().unwrap().clone())),
-                        _ => (),
-                    }
-                }
-            }
-        }
-        Ok(Form { doc, form_ids })
-    }
+    let mut resp_body = vec![0; typebuf.length() as usize];
+    typebuf.copy_to(&mut resp_body[..]);
 
-    /// Returns the number of fields the form has
-    pub fn len(&self) -> usize {
-        self.form_ids.len()
-    }
+    let mut form = Form::load_from(&resp_body[..]).unwrap();
+//
+//    console::log_1(&"THREE".into());
+//
+//    // Manufacture the element we're gonna append
+//    //let body_str: String = resp_body.into_iter().map(|i| i.to_string()).collect::<String>();
+//
+//    let val = document.create_element("p")?;
+//    val.set_inner_html(resp_vec.as_slice().to_hex());
+//    body.append_child(&val)?;
 
-    /// Gets the type of field of the given index
-    ///
-    /// # Panics
-    /// This function will panic if the index is greater than the number of fields
-    pub fn get_type(&self, n: usize) -> FieldType {
-        // unwraps should be fine because load should have verified everything exists
-        let field = self
-            .doc
-            .objects
-            .get(&self.form_ids[n])
-            .unwrap()
-            .as_dict()
-            .unwrap();
-        let obj_zero = Object::Integer(0);
-        let type_str = field.get(b"FT").unwrap().as_name_str().unwrap();
-        if type_str == "Btn" {
-            let flags = ButtonFlags::from_bits_truncate(
-                field.get(b"Ff").unwrap_or(&obj_zero).as_i64().unwrap() as u32,
-            );
-            if flags.intersects(ButtonFlags::RADIO | ButtonFlags::NO_TOGGLE_TO_OFF) {
-                FieldType::Radio
-            } else if flags.intersects(ButtonFlags::PUSHBUTTON) {
-                FieldType::Button
-            } else {
-                FieldType::CheckBox
-            }
-        } else if type_str == "Ch" {
-            let flags = ChoiceFlags::from_bits_truncate(
-                field.get(b"Ff").unwrap_or(&obj_zero).as_i64().unwrap() as u32,
-            );
-            if flags.intersects(ChoiceFlags::COBMO) {
-                FieldType::ComboBox
-            } else {
-                FieldType::ListBox
-            }
-        } else {
-            FieldType::Text
-        }
-    }
+    //console::log_1(&body_str.into());
 
-    /// Gets the name of field of the given index
-    ///
-    /// # Panics
-    /// This function will panic if the index is greater than the number of fields
-    pub fn get_name(&self, n: usize) -> Option<String> {
-        // unwraps should be fine because load should have verified everything exists
-        let field = self
-            .doc
-            .objects
-            .get(&self.form_ids[n])
-            .unwrap()
-            .as_dict()
-            .unwrap();
-
-        // The "T" key refers to the name of the field
-        match field.get(b"T") {
-            Ok(data) => Some(data.clone().as_name_str().unwrap().to_string()),
-            _ => None,
-        }
-    }
-
-    /// Gets the types of all of the fields in the form
-    pub fn get_all_types(&self) -> Vec<FieldType> {
-        let mut res = Vec::with_capacity(self.len());
-        for i in 0..self.len() {
-            res.push(self.get_type(i))
-        }
-        res
-    }
-
-    /// Gets the names of all of the fields in the form
-    pub fn get_all_names(&self) -> Vec<Option<String>> {
-        let mut res = Vec::with_capacity(self.len());
-        for i in 0..self.len() {
-            res.push(self.get_name(i))
-        }
-        res
-    }
-
-    /// If the field at index `n` is a text field, fills in that field with the text `s`.
-    /// If it is not a text field, returns ValueError
-    ///
-    /// # Panics
-    /// Will panic if n is larger than the number of fields
-    pub fn set_text(&mut self, n: usize, s: String) -> Result<(), ValueError> {
-        match self.get_type(n) {
-            FieldType::Text => {
-                let field = self
-                    .doc
-                    .objects
-                    .get_mut(&self.form_ids[n])
-                    .unwrap()
-                    .as_dict_mut()
-                    .unwrap();
-                field.set("V", Object::String(s.into_bytes(), StringFormat::Literal));
-                field.remove(b"AP");
-                Ok(())
-            }
-            _ => Err(ValueError::TypeMismatch),
-        }
-    }
-
-    /// Saves the form to the specified path
-    pub fn save<P: AsRef<Path>>(&mut self, path: P) -> Result<(), io::Error> {
-        self.doc.save(path).map(|_| ())
-    }
-
-    /// Saves the form to the specified path
-    pub fn save_to<W: Write>(&mut self, target: &mut W) -> Result<(), io::Error> {
-        self.doc.save_to(target)
-    }
+    Ok(())
 }
